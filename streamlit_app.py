f159858310cca50b2e3a8c52709a1054c4f2671a
add10052023 <133122254+add10052023@users.noreply.github.com>
2023-05-11 11:38:20 +0100

    Update streamlit_app.py
    added table insert

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,5 @@ streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
 streamlit.text(f"{fruit_choice} has been added")
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
