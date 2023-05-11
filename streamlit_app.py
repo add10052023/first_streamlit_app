de2c9ffe243fb9af212c64a0583fcf756b8b1dd3
add10052023 <133122254+add10052023@users.noreply.github.com>
2023-05-11 09:03:30 +0100

    Update streamlit_app.py
    added default to stop the error message from appearing

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.text('Dietary information...')
 fruits_to_show = my_fruit_list.loc[fruits_selected, ['Serving_Size','Serving_Gram_Weight','Calories', 'Total_Fat_G','Total_Carbs_G)','Sugars_G','Protein_G']]
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 streamlit.dataframe(pandas.json_normalize(fruityvice_response.json()))
 
